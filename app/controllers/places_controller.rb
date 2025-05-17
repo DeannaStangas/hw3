c8cd7bfc03a68672042d128eeb0ff1a170801ad8
Deanna Stangas <deanna.stangas@kellogg.northwestern.edu>
2025-05-17 12:48:09 +0000

    places index and new

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,32 @@
+class PlacesController < ApplicationController
+  def index
+    # find all place rows
+    @places = Place.all
+    # render posts/index view
+  end
+
+  def show
+    # find a Place
+    @place = Place.find_by({"name"=> params["name"]})
+    p @place
+    # render companies/show view with details about Company
+  end
+  
+  def new
+    # render posts/new view with new Post form
+  end
+
+  def create
+    #start a new place
+    @place = Place.new
+    #assign user-entered form data to places column
+    @place["name"] = params["name"]
+    #save place row
+    @place.save
+    #redirect user
+    redirect_to "/places"
+  end
+
+end
+
+
